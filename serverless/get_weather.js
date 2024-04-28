396c27714b4bf0115d25dd5059e06955c64ee32e
smahsan053 <smahsan053@gmail.com>
2024-04-28 20:22:45 +0500

    test01

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -5,6 +5,7 @@ export async function handler(event, context) {
   const params = JSON.parse(event.body);
   const { lat, lon, units } = params;
   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  console.log(url);
   try {
     const weatherStream = await fetch(url);
     const weatherJson = await weatherStream.json();
